fc1de795c466b091e4c849b01ca0ec9df12982d3
Liu Ye <879316283@qq.com>
2018-04-01 00:51:27 +0800

    更新程序集信息

--- SeatKiller_UI/Properties/AssemblyInfo.cs
+++ SeatKiller_UI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SeatKiller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MIT Lisence")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SeatKiller")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: NeutralResourcesLanguage("zh-CN")]
+
