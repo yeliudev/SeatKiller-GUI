32e9e0e983ecb845dd113660ce613b6dccc67a01
Liu Ye <879316283@qq.com>
2018-05-16 01:36:30 +0800

    修复结束时间显示错误的问题

--- SeatKiller_UI/Config.cs
+++ SeatKiller_UI/Config.cs
@@ -101,7 +101,7 @@ namespace SeatKiller_UI
         {
             //About about = new About();
             //about.ShowDialog();
-            MessageBox.Show("版本号：1.8\r\nGitHub仓库：https://github.com/goolhanrry/SeatKiller_UI\r\n还没搭好的个人主页：https://www.goolhanrry.club/\r\n\r\n本软件完全开源，也不会以任何形式收取捐赠\r\nCode Style写得一般，欢迎添加我的微信: aweawds 交流探讨或提交bug ۹(๑•̀ω•́ ๑)۶", "关于");
+            MessageBox.Show("版本号：1.8.1\r\nGitHub仓库：https://github.com/goolhanrry/SeatKiller_UI\r\n还没搭好的个人主页：https://www.goolhanrry.club/\r\n\r\n本软件完全开源，也不会以任何形式收取捐赠\r\nCode Style写得一般，欢迎添加我的微信: aweawds 交流探讨或提交bug ۹(๑•̀ω•́ ๑)۶", "关于");
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
@@ -223,16 +223,27 @@ namespace SeatKiller_UI
             }
             else
             {
-                comboBox4.DataSource = startTimeList;
+                displayStartTimeList = startTimeList;
+                comboBox4.DataSource = displayStartTimeList;
             }
         }
 
         private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
             ArrayList endTimeList = new ArrayList();
-            for (int i = comboBox4.SelectedIndex + 1; i <= displayStartTimeList.Count - 1; i++)
+            if (comboBox4.SelectedIndex == 0 & displayStartTimeList.Count == 30)
             {
-                endTimeList.Add(displayStartTimeList[i]);
+                for (int i = comboBox4.SelectedIndex + 2; i <= displayStartTimeList.Count - 1; i++)
+                {
+                    endTimeList.Add(displayStartTimeList[i]);
+                }
+            }
+            else
+            {
+                for (int i = comboBox4.SelectedIndex + 1; i <= displayStartTimeList.Count - 1; i++)
+                {
+                    endTimeList.Add(displayStartTimeList[i]);
+                }
             }
             endTimeList.Add(new DictionaryEntry("1350", "22:30"));
             comboBox5.DataSource = endTimeList;
