a33c34c6e4f7fc3a590bf30be3f9cab6767647bb
goolhanrry <879316283@qq.com>
2018-10-04 11:48:42 +0800

    “关于”界面调整

--- SeatKiller_UI/About.Designer.cs
+++ SeatKiller_UI/About.Designer.cs
@@ -39,9 +39,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(23, 53);
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(794, 168);
+            this.label1.Size = new System.Drawing.Size(467, 102);
             this.label1.TabIndex = 0;
             this.label1.Text = "版本号：1.8.6\r\nGitHub仓库：\r\n不定时更新，最新版本将在上方GitHub仓库中发布\r\n\r\n本软件完全开源，也不会以任何形式收取捐赠\r\nCode Sty" +
     "le写得一般，欢迎添加我的微信：aweawds 交流探讨或提交bug ۹(๑•̀ω•́ ๑)۶";
@@ -50,9 +51,10 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.linkLabel1.Location = new System.Drawing.Point(154, 81);
+            this.linkLabel1.Location = new System.Drawing.Point(88, 46);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(456, 28);
+            this.linkLabel1.Size = new System.Drawing.Size(261, 17);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/goolhanrry/SeatKiller_UI";
@@ -61,9 +63,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(663, 287);
+            this.button1.Location = new System.Drawing.Point(388, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 53);
+            this.button1.Size = new System.Drawing.Size(89, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
@@ -72,17 +75,18 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 268);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 153);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(860, 92);
+            this.pictureBox1.Size = new System.Drawing.Size(492, 53);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(168F, 168F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(844, 361);
+            this.ClientSize = new System.Drawing.Size(492, 206);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
@@ -90,6 +94,7 @@
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "About";
